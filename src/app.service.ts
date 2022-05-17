00decd5b39f6233d63c3b9857ed876fd700e97f5
Malindu-Upendra <68582673+Malindu-Upendra@users.noreply.github.com>
2022-05-17 21:16:52 +0530

    adding ci/cd pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,23 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Malindu Upendra !';
+    return 'Hello From Malindu Upendra, Testing Docker & Kubernetes';
+    return 'Hello, I am Malindu Upendra. Testing Docker & Kubernetes';
+  }
+
+  getArray(){
+
+    let arr = [{
+      'maths':'A',
+      'Sinhala':'B',
+      'Science':'C'
+    },
+    {
+      'maths':'C',
+      'Sinhala':'A',
+      'Science':'B'
+    }];
+    
+    return arr;
   }
 }
