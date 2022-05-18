50c9f31f7de79f8db00d257b004f177ec56d7906
Malindu-Upendra <68582673+Malindu-Upendra@users.noreply.github.com>
2022-05-18 20:01:15 +0530

    connected with mahir's microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,12 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
   }
 
-  @Get('getNumbers')
-  getNumbers(){
-    return this.appService.getArray();
+  @Get('getProductsFromTheCart')
+  getProducts(){
+    return this.appService.getProductstothePayment();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,24 +1,62 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { Observable } from 'rxjs';
+
+let cart = [
+  {
+    user: 1,
+    products: [1, 2, 3, 4, 5],
+  },
+  {
+    user: 2,
+    products: [4, 5, 6],
+  },
+  {
+    user: 3,
+    products: [1, 3, 5, 7, 9],
+  },
+  {
+    user: 4,
+    products: [10, 9, 8, 7],
+  },
+];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Testing changes on CI/CD pipeline';
+  constructor(private httpService: HttpService) {}
+
+
+  async getHello() {
+    return "Microservice Upendra Y.A.M"
+    //console.log(obj)
   }
 
-  getArray(){
-
-    let arr = [{
-      'maths':'A',
-      'Sinhala':'B',
-      'Science':'C'
-    },
-    {
-      'maths':'C',
-      'Sinhala':'A',
-      'Science':'B'
-    }];
+  async getProductstothePayment() {
+    let items = [];
     
-    return arr;
+    await this.httpService
+      .get('http://34.121.43.145:3000/products')
+      .subscribe(async (result) => console.log(this.getProducts(result.data)));
+
+    console.log(items);
+    return items;
   }
+
+  async getProducts(products: any[]){
+    const user = 1;
+    const cartItems = cart.find((item) => item.user === user);
+    let finalCartItems = [];
+
+    await products.map((item) => {
+      cartItems.products.map((cartI) => {
+        if (item.id === cartI) {
+          finalCartItems.push(item);
+        }
+      });
+    });
+    
+    return finalCartItems;
+  }
+
 }
